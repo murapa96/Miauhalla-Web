21e99317edb6cc7eae03c891c590d45273342e56
Pablo Ramos Muras <pabloramosmuras@gmail.com>
2022-06-13 00:52:59 +0200

    Working Progress

---
+++ README.md
@@ -0,0 +1,4 @@
+<h1 id="argon-design-system"><a href="www.miauhalla.com">Website de Miauhalla Network</a></h1>
+
+<h1 id="argon-design-system"><a href="https://www.creative-tim.com/product/vue-argon-design-system">Basado en Argon Design System</a></h1>
+
---
+++ src/views/components/Hero.vue
@@ -0,0 +1,69 @@
+<template>
+  <section class="section-hero section-shaped my-0">
+    <div class="shape shape-style-1 shape-primary">
+      <img src="img/background/minecraft.jpg" alt="">
+    </div>
+    <div class="container shape-container d-flex align-items-center">
+      <card class="col px-0" style="background-color:rgba(255,255,255,0.6) !important;" hover>
+        <div class="row justify-content-center align-items-center" >
+          <div class="col-lg-7 text-center pt-lg">
+            <img
+              src="img/brand/white.png"
+              style="width: 200px;"
+              class="img-fluid"
+            />
+            <p class="lead text-black mt-4 mb-5" >
+              <b>La Network de Servidores de Videojuegos</b>
+            </p>
+<!--             <div class="btn-wrapper">
+              <base-button
+                tag="a"
+                href="https://www.creative-tim.com/product/vue-argon-design-system"
+                class="mb-3 mb-sm-0"
+                type="white"
+                icon="ni ni-basket-95"
+              >
+                Ir a la tienda
+              </base-button>
+            </div> -->
+          </div>
+        </div>
+        <div
+          class="row align-items-center justify-content-around stars-and-coded"
+        >
+<!--           <div class="col-sm-4">
+            <span class="text-white alpha-7 ml-3">Star us on</span>
+            <a
+              href="https://github.com/creativetimofficial/argon-design-system"
+              target="_blank"
+              title="Support us on Github"
+            >
+              <img
+                src="img/brand/github-white-slim.png"
+                style="height: 22px; margin-top: -3px"
+              />
+            </a>
+          </div>
+          <div class="col-sm-4 mt-4 mt-sm-0 text-right">
+            <span class="text-white alpha-7">Coded by</span>
+            <a
+              href="https://www.creative-tim.com"
+              target="_blank"
+              title="Creative Tim - Premium Bootstrap Themes and Templates"
+            >
+              <img
+                src="img/brand/creativetim-white-slim.png"
+                class="ml-3"
+                style="height: 30px;"
+              />
+            </a>
+          </div> -->
+        </div>
+      </card>
+    </div>
+  </section>
+</template>
+<script>
+export default {};
+</script>
+<style></style>
